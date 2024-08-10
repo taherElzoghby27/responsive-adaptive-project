4037f53c61de83220203abc2c525559642a5b2d5
Taher Elzoghby <tataamen678@gmail.com>
2024-08-10 19:10:54 +0300

    add get responsive font size and get scale factor

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'responsive_text/screen_text.dart';
 import 'task1/view.dart';
+import 'package:device_preview/device_preview.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(
+      DevicePreview(
+        enabled: true,
+        builder: (context) => const MyApp(), // Wrap your app
+      ),
+    );
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -17,7 +22,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ScreenView(),
+      home: const ResponsiveText(),
     );
   }
 }
